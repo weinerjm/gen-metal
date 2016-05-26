53868e467333858b3b8e9d6f391ef8af077c062f
Grundleyump <joshua.m.weiner@colorado.edu>
2016-05-25 23:57:56 -0600

    test db connection logic

--- app.py
+++ app.py
@@ -5,21 +5,20 @@ from randgen import get_lyrics_text, TextGen
 app = Flask(__name__)
 #mongo = PyMongo(app)
 
-
 @app.route("/")
 def main():
-    if 'MONGODB_URI' in os.environ.keys():
-        client = pymongo.MongoClient(os.environ['MONGODB_URI'],
-                                     connectTimeoutMS=30000,
-                                     socketTimeoutMS=None,
-                                     socketKeepAlive=True)
-    else:
+    if 'MONGODB_URI' not in os.environ.keys():
         from mongodb_uri import MONGODB_URI
         client = pymongo.MongoClient(MONGODB_URI,
                                      connectTimeoutMS=30000,
                                      socketTimeoutMS=None,
                                      socketKeepAlive=True)
-    
+    else:
+        client = pymongo.MongoClient(os.environ['MONGODB_URI'],
+                                     connectTimeoutMS=30000,
+                                     socketTimeoutMS=None,
+                                     socketKeepAlive=True)
+
     db = client.get_default_database()
     lyrics_text = get_lyrics_text(db, n_records=20)
     tg = TextGen(lyrics_text)
