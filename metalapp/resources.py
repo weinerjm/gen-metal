59073eba33df591db1241a98c879a202d757f68e
Grundleyump <joshua.m.weiner@colorado.edu>
2016-06-08 10:45:02 -0600

    added regen button and bootstrap.css

--- metalapp/resources.py
+++ metalapp/resources.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, render_template, current_app
+from flask import Flask, request, render_template, current_app, url_for
 from flask.ext.pymongo import PyMongo
 from metalapp import app, mongo
 from randgen import get_lyrics_text, TextGen
 from fix_unicode import fix_bad_unicode
+import ftfy
 
 with app.app_context():
     categories = ['all','judas_priest','highlander',
@@ -15,6 +16,7 @@ with app.app_context():
 @app.route("/")
 def main():
     global tg_dict
+    #display_html = map(lambda x: ftfy.fix_text(x, fix_entities=False), tg_dict['all'].random_text())
     display_html = tg_dict['all'].random_text()
     try:
         pass
@@ -44,7 +46,8 @@ def special_lyrics(category, n):
                                       sort_field=(category,6))
         tg_dict[category] = TextGen(lyrics_text)
     
-    display_html = tg_dict[category].random_text()
+    #display_html = map(lambda x: ftfy.fix_text(x, fix_entities=False), tg_dict[category].random_text())
+    display_html = tg_dict['all'].random_text()
     try:
         pass
         #display_html = fix_bad_unicode(' '.join(display_html).decode('latin9').encode('utf8'))
